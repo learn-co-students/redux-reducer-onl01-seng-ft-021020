017d658d381d4d342d09e2e475b37028b650309b
github username <github email address>
2020-06-24 16:20:42 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
 export function manageFriends(state, action){
+  if (action.type === 'ADD_FRIEND') {
+        
+        let friends = state.friends
+        friends.push(action.friend)
+        return {friends}
+    }
+    else if (action.type === 'REMOVE_FRIEND') {
+      let friends = []
+      state.friends.forEach(element => {
+        if(element.id != action.id){
+          friends.push(element)
+        }
+
+        
+      });
+      
+      return {friends}
+    }
+    else{
+      return {friends: state.friends }
+
+    }
 }
